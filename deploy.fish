c32bf7e9285bde08be2e6ce3ff5d75ec6cbf4b03
Florian Beeres <yuuki@protonmail.com>
2020-05-23 19:34:52 +0200

    add deploy script

---
+++ deploy.fish
@@ -0,0 +1,15 @@
+#!/usr/bin/env fish
+
+zip -r deploy.zip public
+
+if test ! -n "$FBRS_DEPLOY_TOKEN"
+    echo "no FBRS_DEPLOY_TOKEN env var"
+    exit 1
+end
+
+curl -H "Content-Type: application/zip" \
+     -H "Authorization: Bearer $FBRS_DEPLOY_TOKEN" \
+     --data-binary "@deploy.zip" \
+     https://api.netlify.com/api/v1/sites/58ced026-c837-4d6b-9750-56c54bba3e19/deploys
+
+rm deploy.zip
